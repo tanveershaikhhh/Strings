bae11daea791a7b3068e3aab33138a99f7e7b072
tanveershaikhhh <102403198+tanveershaikhhh@users.noreply.github.com>
2023-10-10 07:27:52 +0530

    SubString.java

--- SubString.java
+++ SubString.java
@@ -5,7 +5,11 @@ public class SubString {
 	public static void main(String[] args) {
 		
 		String sentence = "My name is Tanveer Javed Shaikh";
-		System.out.println(sentence.substring(11 , 18)); // always give +1 index cause it is by default set at -1
+		
+		//This will print only the index you give
+		// for eg you can extract a name from sentence
+						//    starting index    End index
+		System.out.println(sentence.substring( 11         ,          18)); // always give +1 index cause it is by default set at -1
 		System.out.println(sentence.substring(11, sentence.length()));
 	}
 
