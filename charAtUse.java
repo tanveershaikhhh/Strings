c1b3adc9c26bd9ca037ccb3cc2903a968f00ceee
tanveershaikhhh <102403198+tanveershaikhhh@users.noreply.github.com>
2023-10-11 11:39:08 +0530

    Add files via upload

---
+++ charAtUse.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class charAtUse {
+	public static void main(String[] args) {
+		
+		String str = "TANVEER";
+		
+		for (int i = 0; i < str.length(); i++) {
+		
+			System.out.println(str.charAt(i));
+			
+		}
+	}
+
+	private static char charAt(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
