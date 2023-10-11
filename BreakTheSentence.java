e99b843f008466ca8c62c929b7773fc245727c0e
tanveershaikhhh <102403198+tanveershaikhhh@users.noreply.github.com>
2023-10-11 11:38:53 +0530

    Add files via upload

---
+++ BreakTheSentence.java
@@ -0,0 +1,28 @@
+package Strings;
+
+import java.util.Scanner;
+
+
+
+public class BreakTheSentence {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String email = sc.next();
+		
+		String username = "";
+		
+		for(int i=0; i<email.length(); i++) {
+			if (email.charAt(i) == '@') {
+				break;
+			}else {
+				username += email.charAt(i);
+			}
+			
+		}
+		System.out.println(username);
+	}
+	
+	
+
+}
